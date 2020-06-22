ba907bd1e192bc46f79ea42830337f1487a5ae39
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-06-22 23:17:43 +0900

    add main, mob, hud

--- player/Player.gd
+++ player/Player.gd
@@ -1,13 +1,15 @@
 extends Area2D
-
+signal hit
 
 export var speed = 400
-var screen_size 
+var screen_size: Vector2 
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
-func _process(delta):
+func _process(delta: float):
+	# 移動に関する制御
 	var velocity : Vector2 = Vector2()
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
@@ -26,3 +28,24 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+
+	# アニメーションに関する制御
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	if velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body):
+	hide();
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", false)
+	
+
+func start(pos: Vector2):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
